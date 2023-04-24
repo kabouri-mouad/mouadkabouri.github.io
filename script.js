d17aaee8792423439c4ff333a9dc1d72fb97b972
KabouriMouad <mouad_kabouri@um5.ac.ma>
2023-04-24 22:04:50 +0100

    added files of my first ePortfolio version

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========================= Typing Animation ======================= */
+var typed = new Typed(".typing", {
+  strings: ["Développeur Web & Mobile.", "Étudiant en génie logiciel.", "Développeur Web & Mobile.", "Résolveur de problèmes", "Passionné de DevOps.", "Enacteur."],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+});
